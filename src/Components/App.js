dac349f7292d3ce2a897b1da7e750b7182e6c51f
DaveCodes <david@codeskill.io>
2017-12-17 22:52:43 -0600

    Basic setup and adding React js

---
+++ src/Components/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+export default class App extends Component {
+  render() {
+    return <h1>Hello World of Electron!!!!</h1>;
+  }
+}
